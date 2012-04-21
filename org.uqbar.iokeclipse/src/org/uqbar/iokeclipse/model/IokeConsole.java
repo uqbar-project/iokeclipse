a38b5fd7e91014c52f900c9b411a4a38efef2f8d
javier.fernandes@gmail.com <javier.fernandes@gmail.com@f54befd8-3c30-16fc-dd4c-6dd0bc8eed9f>
2012-04-21 22:14:58 +0000

    adding a real rought implementation of the commands history for the console

--- org.uqbar.iokeclipse/src/org/uqbar/iokeclipse/model/IokeConsole.java
+++ org.uqbar.iokeclipse/src/org/uqbar/iokeclipse/model/IokeConsole.java
@@ -1,50 +1,92 @@
 package org.uqbar.iokeclipse.model;
 
+import ioke.lang.Runtime;
+import ioke.lang.exceptions.ControlFlow;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+
+import org.uqbar.iokeclipse.IokeActivator;
 
 /**
  * 
  * @author jfernandes
  */
 public class IokeConsole extends OutputStream {
-    private int maxSize = 80000;
-    private int clearOnMaxFactor = 2;
-    private StringBuffer buffer = new StringBuffer();
-    private List<IokeConsoleListener> listeners = new ArrayList<IokeConsoleListener>(); 
+	private int maxSize = 80000;
+	private int clearOnMaxFactor = 2;
+	private StringBuffer buffer = new StringBuffer();
+	private List<IokeConsoleListener> listeners = new ArrayList<IokeConsoleListener>();
+	private List<String> commandsHistory = new LinkedList<String>();
 
-    @Override
-    public void write(int b) throws IOException {
-	this.checkMaxToCut();
-	buffer.append(Character.toString((char) b));
-	this.fireChanged();
-    }
-
-    protected void fireChanged() {
-	for (IokeConsoleListener listener : this.listeners) {
-	    listener.consoleChanged(this.buffer);
+	@Override
+	public void write(int b) throws IOException {
+		this.checkMaxToCut();
+		buffer.append(Character.toString((char) b));
+		this.fireChanged();
 	}
-    }
-    
-    private void checkMaxToCut() {
-	if (this.buffer.length() >= maxSize) {
-	    this.buffer.delete(0, this.maxSize / this.clearOnMaxFactor);
+	
+	public Object evaluate(String text) throws ControlFlow {
+		Object result = this.getRuntime().evaluateString(text + "\n");
+		this.commandsHistory.add(text);
+		return result;
 	}
-    }
+	
+	public List<String> getCommandsHistory() {
+		return this.commandsHistory;
+	}
+	
+	protected Runtime getRuntime() {
+		return IokeActivator.getDefault().getRuntime();
+	}
 
-    public void clear() {
-	this.buffer.setLength(0);
-	this.fireChanged();
-    }
-    
-    public void addListener(IokeConsoleListener listener) {
-	this.listeners.add(listener);
-    }
-    
-    public void removeListener(IokeConsoleListener listener) {
-	this.listeners.remove(listener);
-    }
+
+	private void checkMaxToCut() {
+		if (this.buffer.length() >= maxSize) {
+			this.buffer.delete(0, this.maxSize / this.clearOnMaxFactor);
+		}
+	}
+
+	public void clear() {
+		this.buffer.setLength(0);
+		this.fireChanged();
+	}
+	
+	// listeners
+	
+	protected void fireChanged() {
+		for (IokeConsoleListener listener : this.listeners) {
+			listener.consoleChanged(this.buffer);
+		}
+	}
+
+	public void addListener(IokeConsoleListener listener) {
+		this.listeners.add(listener);
+	}
+
+	public void removeListener(IokeConsoleListener listener) {
+		this.listeners.remove(listener);
+	}
+
+	private int historyIndex = 0;
+	
+	public String moveHistoryUp() {
+		return moveHistory(1);
+	}
+
+	public String moveHistoryDown() {
+		return moveHistory(-1);
+	}
+	
+	private String moveHistory(int delta) {
+		String historyItem = this.commandsHistory.get(this.commandsHistory.size() - 1 - this.historyIndex);
+		this.historyIndex += delta;
+		this.historyIndex = Math.max(this.historyIndex, 0);
+		this.historyIndex = Math.min(this.historyIndex, this.commandsHistory.size() - 1);
+		return historyItem;
+	}
 
 }
--- org.uqbar.iokeclipse/src/org/uqbar/iokeclipse/views/console/IokeConsoleView.java
+++ org.uqbar.iokeclipse/src/org/uqbar/iokeclipse/views/console/IokeConsoleView.java
@@ -1,19 +1,18 @@
 package org.uqbar.iokeclipse.views.console;
 
-import ioke.lang.Runtime;
 import ioke.lang.exceptions.ControlFlow;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.jface.action.Separator;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.TraverseEvent;
 import org.eclipse.swt.events.TraverseListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
@@ -26,97 +25,111 @@ import org.uqbar.iokeclipse.model.IokeConsoleListener;
  * @author jfernandes
  */
 public class IokeConsoleView extends ViewPart implements IokeConsoleListener {
-    public static final String ID = "org.uqbar.iokeclipse.views.IokeConsoleView";
-    private StyledText inputTextArea;
-    private StyledText consoleTextArea;
+	public static final String ID = "org.uqbar.iokeclipse.views.IokeConsoleView";
+	private StyledText inputTextArea;
+	private StyledText consoleTextArea;
 
-    public void createPartControl(Composite parent) {
-	parent.setLayout(new org.eclipse.swt.layout.GridLayout(1, true));
-	this.createInputTextArea(parent);
-	this.createTextArea(parent);
-	this.contributeToActionBars();
-    }
-
-    protected void createInputTextArea(Composite parent) {
-	this.inputTextArea = new StyledText(parent, SWT.BORDER | SWT.SINGLE);
-	this.inputTextArea.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL));
-	this.inputTextArea.addTraverseListener(new TraverseListener() {
-	    @Override
-	    public void keyTraversed(TraverseEvent e) {
-		if (e.detail == SWT.TRAVERSE_RETURN) {
-		    evaluateInput();
-		}
-	    }
-	});
-    }
-
-    protected Runtime getRuntime() {
-	return IokeActivator.getDefault().getRuntime();
-    }
-
-    protected void evaluateInput() {
-	try {
-	    Object result = getRuntime().evaluateString(this.inputTextArea.getText() + "\n");
-	    this.inputTextArea.setText("");
-	    this.inputTextArea.setSelection(0);
-	    this.consoleTextArea.setText(this.consoleTextArea.getText() + "\n" + result);
-	} catch (ControlFlow e) {
-	    this.consoleTextArea.setText(this.consoleTextArea.getText() + "\n" + e.getMessage());
+	public void createPartControl(Composite parent) {
+		parent.setLayout(new org.eclipse.swt.layout.GridLayout(1, true));
+		this.createInputTextArea(parent);
+		this.createTextArea(parent);
+		this.contributeToActionBars();
 	}
-    }
 
-    protected void createTextArea(Composite parent) {
-	this.consoleTextArea = new StyledText(parent, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
-	this.consoleTextArea.setLayoutData(new GridData(GridData.FILL_BOTH | GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL));
-	this.consoleTextArea.setEditable(false);
+	protected void createInputTextArea(Composite parent) {
+		this.inputTextArea = new StyledText(parent, SWT.BORDER | SWT.SINGLE);
+		this.inputTextArea.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL));
+		this.inputTextArea.addKeyListener(new KeyListener() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.keyCode == SWT.ARROW_UP) {
+					inputTextArea.setText(getConsole().moveHistoryUp());
+				}
+				else if (e.keyCode == SWT.ARROW_DOWN) {
+					inputTextArea.setText(getConsole().moveHistoryDown());
+				}
+				e.doit = true;
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+			}
+		});
+		this.inputTextArea.addTraverseListener(new TraverseListener() {
+			@Override
+			public void keyTraversed(TraverseEvent e) {
+				if (e.detail == SWT.TRAVERSE_RETURN) {
+					evaluateInput();
+				}
+			}
+		});
+	}
 	
-	this.getConsole().addListener(this);
-    }
+	protected void evaluateInput() {
+		try {
+			Object result = getConsole().evaluate(inputTextArea.getText());
+			this.inputTextArea.setText("");
+			this.inputTextArea.setSelection(0);
+			this.consoleTextArea.setText(this.consoleTextArea.getText() + "\n" + result);
+		} catch (ControlFlow e) {
+			this.consoleTextArea.setText(this.consoleTextArea.getText() + "\n" + e.getMessage());
+		}
+	}
 
-    protected IokeConsole getConsole() {
-	return IokeActivator.getDefault().getConsole();
-    }
-    
-    @Override
-    public void setFocus() {
-	this.inputTextArea.setFocus();
-    }
+	protected void createTextArea(Composite parent) {
+		this.consoleTextArea = new StyledText(parent, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
+		this.consoleTextArea.setLayoutData(new GridData(GridData.FILL_BOTH | GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL));
+		this.consoleTextArea.setEditable(false);
 
-    @Override
-    public void consoleChanged(StringBuffer buffer) {
-	// this is totally non-performant. We should connecto the stream directly to the StyledTextContent
-	// but just for now, as a prototype
-	this.consoleTextArea.setText(buffer.toString());
-    }
-    
-    @Override
-    public void dispose() {
-	getConsole().removeListener(this);
-	super.dispose();
-    }
-    
-    // *****************************************
-    // ** menus and actions
-    // *****************************************  
+		this.getConsole().addListener(this);
+	}
 
-    protected void contributeToActionBars() {
-	this.fillLocalToolBar(getViewSite().getActionBars().getToolBarManager());
-    }
-    
-    protected void fillLocalToolBar(IToolBarManager manager) {
-	manager.add(this.createClearConsoleAction());
-    }
+	protected IokeConsole getConsole() {
+		return IokeActivator.getDefault().getConsole();
+	}
 
-    protected IAction createClearConsoleAction() {
-	Action action = new Action() {
-	    public void run() {
-		getConsole().clear();
-	    }
-	};
-	action.setText("Clear");
-	action.setToolTipText("Clear Console Text");
-	action.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_DELETE));
-	return action;
-    }
-    
+	@Override
+	public void setFocus() {
+		this.inputTextArea.setFocus();
+	}
+
+	@Override
+	public void consoleChanged(StringBuffer buffer) {
+		// this is totally non-performant. We should connecto the stream
+		// directly to the StyledTextContent
+		// but just for now, as a prototype
+		this.consoleTextArea.setText(buffer.toString());
+	}
+
+	@Override
+	public void dispose() {
+		this.getConsole().removeListener(this);
+		super.dispose();
+	}
+
+	// *****************************************
+	// ** menus and actions
+	// *****************************************
+
+	protected void contributeToActionBars() {
+		this.fillLocalToolBar(getViewSite().getActionBars().getToolBarManager());
+	}
+
+	protected void fillLocalToolBar(IToolBarManager manager) {
+		manager.add(this.createClearConsoleAction());
+	}
+
+	protected IAction createClearConsoleAction() {
+		Action action = new Action() {
+			public void run() {
+				getConsole().clear();
+			}
+		};
+		action.setText("Clear");
+		action.setToolTipText("Clear Console Text");
+		action.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_TOOL_DELETE));
+		return action;
+	}
+
 }
